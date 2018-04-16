e23c30f4104274f3ac006cb7a687cfffb6065fef
Brandon Poythress <bgpoythress@yahoo.com>
2018-04-16 06:56:47 -0400

    Working on the renderer

--- Renderer.js
+++ Renderer.js
@@ -66,7 +66,7 @@ function Renderer(gl){
 	
 }	
 	
-	
+//initialize the renderer	
 Renderer.prototype.init = function(gl){
 	
 	//Initialize shaders
@@ -81,8 +81,69 @@ Renderer.prototype.init = function(gl){
 
 }
 
+//------------------------------------------------------------------------------------
+//This set of functions is the meat an potatoes of the renderer.  It  searches through
+//the data structures of the a given state and decides how to render each item.
+Renderer.prototype.render = function(gl, state){
+	var stateRenderListLength = state.renderList.length;
+	for (var i=0; i<stateRenderListLength; i++){
+		if (state.renderList[i].type = "Scene"){
+			this.renderScene(state.renderList[i]);
+		} else if (state.renderList[i].type = "Portal"){
+			this.renderPortal(state.renderList[i]);
+		}else if (state.renderList[i].type = "Sketch"){
+			this.renderSketch(state.renderList[i]);
+		}else if (state.renderList[i].type = "Plane"){
+			this.renderPlane(state.renderList[i]);
+		}else if (state.renderList[i].type = "Line"){
+			this.renderLine(state.renderList[i]);
+		}else if (state.renderList[i].type = "Point"){
+			this.renderPoint(state.renderList[i]);
+		}else{
 
+		}
+}
+
+Renderer.prototype.renderScene = function(gl, scene){
+	var sceneRenderListLength = scene.renderList.length;
+	for (var i=0; i<sceneRenderListLength; i++){
+		if (scene.renderList[i].type = "Portal"){
+			this.renderPortal(scene.renderList[i]);
+		}else if (scene.renderList[i].type = "Sketch"){
+			this.renderSketch(scene.renderList[i]);
+		}else if (scene.renderList[i].type = "Plane"){
+			this.renderPlane(scene.renderList[i]);
+		}else if (scene.renderList[i].type = "Line"){
+			this.renderLine(scene.renderList[i]);
+		}else if (scene.renderList[i].type = "Point"){
+			this.renderPoint(scene.renderList[i]);
+		}else{
+
+		}
+	}
+}
+
+Renderer.prototype.renderPortal = function(gl, portal){
 	
+}
+
+Renderer.prototype.renderSketch = function(gl, sketch){
+	
+}
+
+Renderer.prototype.renderPlane = function(gl, plane){
+	
+}
+
+Renderer.prototype.renderLine = function(gl, line){
+	
+}
+
+Renderer.prototype.renderPoint = function(gl, point){
+	
+}
+
+//end of meat and potatoes--------------------------------------------------------------	
 
 Renderer.prototype.drawLine = function(gl, vertexColorBuffer, vertSize, colorSize, FSIZE, n){
 
